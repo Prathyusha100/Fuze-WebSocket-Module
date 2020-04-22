ffd61028e199b98a964cfc38adb6d14ff2d4c4cf
Prathyusha Thangella <prathyusha.t@BRILLIO.COM>
2020-04-22 16:28:02 +0530

    Added all projects from Old websocket Repo

---
+++ FuzeAppWebsocket/src/main/java/com/websocket/FuzeAppWebsocket/FuzeAppWebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.websocket.FuzeAppWebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzeAppWebsocketApplication {
+
+	public static void main(String[] args) {
+		System.out.println("FuzeAppWebsocket Application Started");
+		SpringApplication.run(FuzeAppWebsocketApplication.class, args);
+	}
+
+}
