0f885da7c671b07c2297dbf9edc93dbc37b9b7a2
Prathyusha Thangella <prathyusha.t@BRILLIO.COM>
2020-04-22 19:55:13 +0530

    deleted Fuze sites project

--- FuzeSites/src/main/java/com/application/site/repository/PoRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.site.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.site.entity.PoRequest;
-
-public interface PoRequestRepository extends JpaRepository<PoRequest, Integer> {
-
-}
--- FuzeSites/src/main/java/com/application/site/repository/SiteProjectsFieldChangesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.site.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.site.entity.SiteProjectsFieldChanges;
-
-public interface SiteProjectsFieldChangesRepository extends JpaRepository<SiteProjectsFieldChanges, Integer> {
-
-}
